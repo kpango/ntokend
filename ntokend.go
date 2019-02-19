7c9c5f713dd8b724788e4f9617c8b003d76369e2
kpango <i.can.feel.gravity@gmail.com>
2019-02-19 17:11:03 +0900

    fix

---
+++ ntokend.go
@@ -0,0 +1,314 @@
+package ntokend
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"sync/atomic"
+	"time"
+	"unsafe"
+
+	"github.com/kpango/glg"
+	"github.com/pkg/errors"
+	"github.com/yahoo/athenz/libs/go/zmssvctoken"
+)
+
+// TokenService represents a interface for user to get the token, and automatically update the token
+type TokenService interface {
+	StartTokenUpdater(context.Context) TokenService
+	TokenExists() bool
+	GetTokenProvider() TokenProvider
+}
+
+type token struct {
+	tokenFilePath   string
+	token           *atomic.Value
+	validateToken   bool
+	tokenExpiration time.Duration
+	refreshDuration time.Duration
+	builder         zmssvctoken.TokenBuilder
+
+	// token builder parameters
+	athenzDomain string
+	serviceName  string
+	keyVersion   string
+	keyData      []byte
+	hostname     string
+	ipAddr       string
+}
+
+type rawToken struct {
+	domain     string
+	name       string
+	signature  string
+	expiration time.Time
+}
+
+var (
+	// ErrTokenNotFound represents a error the the token is not found
+	ErrTokenNotFound = errors.New("Error:\ttoken not found")
+)
+
+// TokenProvider represents a token provider function to get the role token
+type TokenProvider func() (string, error)
+
+// tokenOption represents a functional options pattern interface
+type tokenOption func(*token) error
+
+// TokenFilePath represents a functional options pattern setter method to set the token file path value
+func TokenFilePath(path string) tokenOption {
+	return func(tok *token) error {
+		tok.tokenFilePath = path
+		return nil
+	}
+}
+
+// EnableValidate represents a functional options pattern setter method to enable validate token flag value
+func EnableValidate() tokenOption {
+	return func(tok *token) error {
+		tok.validateToken = true
+		return nil
+	}
+}
+
+// DisableValidate represents a functional options pattern setter method to disable validate token flag value
+func DisableValidate() tokenOption {
+	return func(tok *token) error {
+		tok.validateToken = false
+		return nil
+	}
+}
+
+// TokenExpiration represents a functional options pattern setter method to set the token expiration period
+func TokenExpiration(dur time.Duration) tokenOption {
+	return func(tok *token) error {
+		tok.tokenExpiration = dur
+		return nil
+	}
+}
+
+// RefreshDuration represents a functional options pattern setter method to set the token refresh duration
+func RefreshDuration(dur time.Duration) tokenOption {
+	return func(tok *token) error {
+		tok.refreshDuration = dur
+		return nil
+	}
+}
+
+// AthenzDomain represents a functional options pattern setter method to set the domain name of the token builder
+func AthenzDomain(domain string) tokenOption {
+	return func(tok *token) error {
+		tok.athenzDomain = domain
+		return nil
+	}
+}
+
+// ServiceName represents a functional options pattern setter method to set the service name of the token builder
+func ServiceName(name string) tokenOption {
+	return func(tok *token) error {
+		tok.serviceName = name
+		return nil
+	}
+}
+
+// KeyVersion represents a functional options pattern setter method to set the key version of the token builder
+func KeyVersion(ver string) tokenOption {
+	return func(tok *token) error {
+		tok.keyVersion = ver
+		return nil
+	}
+}
+
+// KeyData represents a functional options pattern setter method to set the key data of the token builder
+func KeyData(keyData []byte) tokenOption {
+	return func(tok *token) error {
+		tok.keyData = keyData
+		return nil
+	}
+}
+
+// Hostname represents a functional options pattern setter method to set the hostname of the token builder
+func Hostname(hostname string) tokenOption {
+	return func(tok *token) error {
+		tok.hostname = hostname
+		return nil
+	}
+}
+
+// IPAddr represents a functional options pattern setter method to set the IP address of the token builder
+func IPAddr(ipAddr string) tokenOption {
+	return func(tok *token) error {
+		tok.ipAddr = ipAddr
+		return nil
+	}
+}
+
+// New return TokenService
+// This function will initialize the TokenService object with the tokenOptions
+func New(tokenOptions ...tokenOption) (TokenService, error) {
+	tok := &token{
+		token: new(atomic.Value),
+	}
+
+	for _, to := range tokenOptions {
+		if err := to(tok); err != nil {
+			return nil, err
+		}
+	}
+
+	// create token builder
+	tokBuilder, err := zmssvctoken.NewTokenBuilder(tok.athenzDomain, tok.serviceName, tok.keyData, tok.keyVersion)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create ZMS SVC Token Builder\nAthenzDomain:\t%s\nServiceName:\t%s\nKeyVersion:\t%s\nError: %v", tok.athenzDomain, tok.serviceName, tok.keyVersion, err)
+	}
+	if tok.hostname != "" {
+		tokBuilder.SetHostname(tok.hostname)
+	}
+	if tok.ipAddr != "" {
+		tokBuilder.SetIPAddress(tok.ipAddr)
+	}
+
+	tok.builder = tokBuilder
+
+	return tok, nil
+}
+
+// StartTokenUpdater return TokenService
+// This function will start a goroutine to update the token periodically, and store the token into memory
+func (t *token) StartTokenUpdater(ctx context.Context) TokenService {
+	go func() {
+		var err error
+		err = t.update()
+		if err != nil {
+			glg.Error(err)
+		}
+
+		ticker := time.NewTicker(t.refreshDuration)
+		for {
+			select {
+			case <-ctx.Done():
+				ticker.Stop()
+				return
+			case <-ticker.C:
+				err = t.update()
+				if err != nil {
+					glg.Error(err)
+				}
+			}
+		}
+	}()
+	return t
+}
+
+// GetTokenProvider returns a function pointer to get the token.
+func (t *token) GetTokenProvider() TokenProvider {
+	return t.getToken
+}
+
+// GetToken return a token string or error
+// This function is thread-safe. This function will return the token stored in the atomic variable, or return the error when the token is not initialized or cannot be generated
+func (t *token) getToken() (string, error) {
+	tok := t.token.Load()
+	if tok == nil {
+		return "", ErrTokenNotFound
+	}
+	return tok.(string), nil
+}
+
+func (t *token) TokenExists() bool {
+	_, err := t.getToken()
+	return err == nil
+}
+
+// loadToken return a n-token string, or error
+// This function return n-token, which is generated with the token builder. If the ntoken_path is set in the yaml (Copper Argos),
+// this function will directly return the token file content.
+// If ntoken_path is not set (k8s secret), the builder will read the private key from environment variable (private_key_env_name), and generate and sign a new token and return.
+// This function can also validate the token generated or read. If validate_token flag is on, this function will verify the token first before this function return.
+func (t *token) loadToken() (ntoken string, err error) {
+	if t.tokenFilePath == "" {
+		// k8s secret
+		t.builder.SetExpiration(t.tokenExpiration)
+
+		ntoken, err = t.builder.Token().Value()
+		if err != nil {
+			return "", err
+		}
+
+	} else {
+		// Copper Argos
+		var tok []byte
+		tok, err = ioutil.ReadFile(t.tokenFilePath)
+		if err != nil {
+			return "", err
+		}
+
+		ntoken = strings.TrimRight(*(*string)(unsafe.Pointer(&tok)), "\r\n")
+	}
+
+	if t.validateToken {
+		err = newRawToken(ntoken).isValid()
+		if err != nil {
+			return "", fmt.Errorf("invalid server identity token:\t%s", err.Error())
+		}
+	}
+	return ntoken, nil
+}
+
+func (t *token) update() error {
+	token, err := t.loadToken()
+	if err != nil {
+		return err
+	}
+	t.setToken(token)
+	return nil
+}
+
+func (t *token) setToken(token string) {
+	t.token.Store(token)
+}
+
+// newRawToken returns the rawToken pointer.
+// This function parse the token string, and transform to rawToken struct.
+func newRawToken(token string) *rawToken {
+	t := new(rawToken)
+	for _, field := range strings.Split(token, ";") {
+		parts := strings.SplitN(field, "=", 2)
+		if len(parts) != 2 {
+			continue
+		}
+		switch parts[0] {
+		case "d": // Domain
+			t.domain = parts[1]
+		case "n": // Name
+			t.name = parts[1]
+		case "s": // Signature
+			t.signature = parts[1]
+		case "e": // Expiration
+			parsed, err := strconv.ParseInt(parts[1], 0, 64)
+			if err != nil {
+				t.expiration = time.Now().Add(time.Second * 30)
+			} else {
+				t.expiration = time.Unix(parsed, 0)
+			}
+		}
+	}
+	return t
+}
+
+// isValid returns error from validating the rawToken struct.
+func (r *rawToken) isValid() error {
+	switch {
+	case r.domain == "":
+		return fmt.Errorf("no domain in token")
+	case r.name == "":
+		return fmt.Errorf("no name in token")
+	case r.signature == "":
+		return fmt.Errorf("no signature in token")
+	case r.expiration.Before(time.Now()):
+		return fmt.Errorf("token has expired")
+	}
+	return nil
+}
